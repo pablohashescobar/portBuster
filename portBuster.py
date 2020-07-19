e6ce09c4645603ae6562fc617f195dc47a6546bc
Nikhil <nikhil13012000@gmail.com>
2020-07-19 23:18:28 +0530

    time added

--- portBuster.py
+++ portBuster.py
@@ -2,23 +2,49 @@ import socket
 import sys
 import threading
 from queue import Queue
+import time
 
 host = '10.10.88.120'
 
-
-def scanner():
-    for port in range(1, 65535):
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        try:
-            con = s.connect((host, port))
-            if con == 0:
-                print(f'Port {port} is open')
-            s.close()
-        except socket.error:
-            pass
-        except KeyboardInterrupt:
-            print('Quitting....')
-            sys.exit()
+print_lock = threading.Lock()
 
 
-scanner()
+def scanner(port):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    try:
+        con = s.connect((host, port))
+
+        with print_lock:
+            print('port', port)
+        con.close()
+    except:
+        pass
+
+
+def threader():
+    while True:
+        worker = q.get()
+        scanner(worker)
+        q.task_done()
+
+
+q = Queue()
+
+start = time.time()
+
+for x in range(100):
+    t = threading.Thread(target=threader)
+    t.daemon = True
+    t.start()
+
+for worker in range(1, 100):
+    q.put(worker)
+
+q.join()
+
+end = time.time()
+
+totalTime = end - start
+
+print(f"Scan completed in {round(totalTime)} second(s)")
